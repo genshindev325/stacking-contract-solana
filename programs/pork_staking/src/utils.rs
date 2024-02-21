f691867299ea2fe6b2db2b30c15b1a5cba18287d
genshindev325 <genshindev325@gmail.com>
2024-02-21 14:23:40 -0500

    update

--- programs/pork_staking/src/utils.rs
+++ programs/pork_staking/src/utils.rs
@@ -9,15 +9,11 @@ pub const PORK_MINT_ADDRESS: Pubkey = pubkey!("2kSmCB5PrswNvN5vrN4ayb2DnVbeFmNhX
 pub const BIGGER_HOLDER: u64 = 10_000_000_000_000_000;
 pub const MINIMUM_DEPOSIT: u64 = 10_000_000_000_000;
 
-pub fn calculate_rewards(
-    amount: u64,
-    last_deposit_timestamp: i64,
-    current_timestamp: i64,
-) -> u64 {
+pub fn calculate_rewards(amount: u64, last_deposit_timestamp: i64, current_timestamp: i64) -> u64 {
     let time_diff: u64 = (current_timestamp - last_deposit_timestamp)
         .try_into()
         .unwrap();
-    let reward: u64 = ((amount * PERCENT / 100) * time_diff / (60 * 60 * 24))
+    let reward: u64 = ((amount * PERCENT / (100 * 60 * 60 * 24)) * time_diff)
         .try_into()
         .unwrap();
     msg!(&(reward).to_string());
@@ -34,9 +30,9 @@ pub fn calculate_bigger_holder_rewards(
     let time_diff: u64 = (current_timestamp - bigger_holder_timestamp)
         .try_into()
         .unwrap();
-    let reward: u64 = ((tvl * BIGGER_HOLDER_PERCENT * times_of_bigger_holder / 100_000)
-        * time_diff
-        / (60 * 60 * 24))
+    let reward: u64 = ((tvl * BIGGER_HOLDER_PERCENT * times_of_bigger_holder
+        / (100_000 * 60 * 60 * 24))
+        * time_diff)
         .try_into()
         .unwrap();
     msg!(&(reward).to_string());
